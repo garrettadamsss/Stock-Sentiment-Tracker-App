bd166cea0905fd550df5623378f031967263f227
Vincent Lee <vinnyclee@gmail.com>
2022-11-26 22:07:50 -0800

    set up retrofit

--- app/src/main/java/com/cs4750/stocksentimenttracker/api/RedditApi.kt
+++ app/src/main/java/com/cs4750/stocksentimenttracker/api/RedditApi.kt
@@ -1,8 +1,11 @@
 package com.cs4750.stocksentimenttracker.api
 
-import okhttp3.Call
+
+import retrofit2.Call
 import retrofit2.http.GET
 
+
+//add here to set up and authorize reddit api
 interface RedditApi {
     @GET("/")
     fun fetchContents(): Call<String>
