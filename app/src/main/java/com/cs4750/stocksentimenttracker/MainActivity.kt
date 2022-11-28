9ac74400cc67f7452eb96a29a04712b65ef0f807
Garrett Adams <Garrett Adams>
2022-11-28 13:35:26 -0800

    setting up retrofit

--- app/src/main/java/com/cs4750/stocksentimenttracker/MainActivity.kt
+++ app/src/main/java/com/cs4750/stocksentimenttracker/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.rvLeaderboard, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
+
     }
+
+
 }
