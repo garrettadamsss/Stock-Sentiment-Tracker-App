1fa3bf8c2bcf69f901053f35c8c2a9f258ec3961
Garrett Adams <Garrett Adams>
2022-11-30 01:24:02 -0800

    setup retrofit and recycler view

--- app/src/main/java/com/cs4750/stocksentimenttracker/api/RedditApi.kt
+++ app/src/main/java/com/cs4750/stocksentimenttracker/api/RedditApi.kt
@@ -1,9 +1,10 @@
 package com.cs4750.stocksentimenttracker.api
 
-import okhttp3.Call
+import retrofit2.Call
 import retrofit2.http.GET
 
+
 interface RedditApi {
-//    @GET("/")
-//    fun fetchContents(): Call<String>
+    @GET("/r/wallstreetbets/.json")
+    fun fetchContents(): Call<String>
 }
